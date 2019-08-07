0d25b087ac39b0c76bf6045c41893a3d32e7b159
luzimar <lzm.spfc@hotmail.com>
2019-08-07 20:17:46 -0300

    Criado loader de users

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,11 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) =>
-  res.json({
-    message: `Hello world ${process.env.DIALECT_DB}`,
-  })
-);
+routes.get('/', async (req, res) => {
+  const user = await User.findAll();
+  return res.json(user);
+});
 
 export default routes;
