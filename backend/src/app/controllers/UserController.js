826a565b052a5f9cc074be25a4afa72d3aefbba3
luzimar <lzm.spfc@hotmail.com>
2019-08-07 23:57:49 -0300

    implementado atualização de usuário

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -19,6 +19,41 @@ class UserController {
       email,
     });
   }
+
+  async update(req, res) {
+    const { email, oldPassword, password } = req.body;
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists.' });
+      }
+    }
+
+    if (password && !oldPassword) {
+      return res.status(401).json({ error: 'Old password not provided' });
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    try {
+      const { id, name } = await user.update(req.body);
+
+      return res.json({
+        id,
+        name,
+        email,
+      });
+    } catch (error) {
+      return res
+        .status(400)
+        .json({ error: 'An error occurred while updating user :(' });
+    }
+  }
 }
 
 export default new UserController();
