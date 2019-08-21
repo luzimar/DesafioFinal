0a8c34e647f85bb8621bf4bb237d532da09d7b44
luzimar <lzm.spfc@hotmail.com>
2019-08-21 20:16:57 -0300

    Correções

--- backend/src/app/jobs/SubscriptionMail.js
+++ backend/src/app/jobs/SubscriptionMail.js
@@ -7,19 +7,19 @@ class SubscriptionMail {
     return process.env.SUBSCRIPTION_KEY;
   }
 
-  // async handle({ data }) {
-  //   const { meetup, user } = data;
-  //   await Mail.sendMail({
-  //     to: `${meetup.organizer.name} <${meetup.organizer.email}>`,
-  //     subject: 'Nova inscrição',
-  //     template: 'subscription',
-  //     context: {
-  //       organizer: meetup.organizer.name,
-  //       user: user.name,
-  //       userEmail: user.email,
-  //     },
-  //   });
-  // }
+  async handle({ data }) {
+    const { meetup, user } = data;
+    await Mail.sendMail({
+      to: `${meetup.organizer.name} <${meetup.organizer.email}>`,
+      subject: 'Nova inscrição',
+      template: 'subscription',
+      context: {
+        organizer: meetup.organizer.name,
+        user: user.name,
+        userEmail: user.email,
+      },
+    });
+  }
 }
 
 export default new SubscriptionMail();
