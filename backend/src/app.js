faf694117f489065c9142d700bfeb14949f20957
luzimar <lzm.spfc@hotmail.com>
2019-08-21 21:56:31 -0300

    Configurado sentry e youch para tratamento de erros

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,17 +1,29 @@
+import 'dotenv/config';
+
 import express from 'express';
 import path from 'path';
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
+import 'express-async-errors';
+import cors from 'cors';
+import sentryConfig from './config/sentry';
 import routes from './routes';
-
 import './database';
 
 class App {
   constructor() {
     this.server = express();
+
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(cors());
+    this.server.use(Sentry.Handlers.requestHandler());
     // permitindo receber requisições e retornar respostas no formato json
     this.server.use(express.json());
     this.server.use(
@@ -22,6 +34,19 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      if (process.env.NODE_ENV === 'development') {
+        const errors = await new Youch(err, req).toJSON();
+
+        return res.status(500).json(errors);
+      }
+
+      return res.status(500).json({ error: 'Internal server error' });
+    });
   }
 }
 
