082881640ad8e195ca22b24af8288f33f3b0815a
luzimar <lzm.spfc@hotmail.com>
2019-08-25 12:21:57 -0300

    Definido estilos globais da aplicação

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,12 @@ import { Router } from 'react-router-dom';
 import Routes from './routes';
 import history from './services/history';
 import './config/reactotron';
+import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <Router history={history}>
+      <GlobalStyle />
       <Routes />
     </Router>
   );
--- frontend/src/routes/Route.js
+++ frontend/src/routes/Route.js
@@ -2,39 +2,50 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
 
+import AuthLayout from '../pages/_layouts/auth';
+import DefaultLayout from '../pages/_layouts/default';
+
 export default function RouteWrapper({
   component: Component,
   isPrivate,
   isNotFound,
   ...rest
-}){
+}) {
   const signed = false;
 
-  if(isNotFound){
-    return <Route { ...rest } component={Component}/>
+  if (isNotFound) {
+    return <Route {...rest} component={Component} />;
   }
-  if(!signed && isPrivate)
-  {
-    return <Redirect to="/"/>
-  }
-
-  if(signed && !isPrivate)
-  {
-    return <Redirect to="/dashboard"/>
+  if (!signed && isPrivate) {
+    return <Redirect to="/" />;
   }
 
-  return <Route { ...rest } component={Component}/>
+  if (signed && !isPrivate) {
+    return <Redirect to="/dashboard" />;
+  }
+
+  const Layout = signed ? DefaultLayout : AuthLayout;
+
+  return (
+    <Route
+      {...rest}
+      render={props => (
+        <Layout>
+          <Component {...props} />
+        </Layout>
+      )}
+    />
+  );
 }
-
 
 RouteWrapper.propTypes = {
   isPrivate: PropTypes.bool,
   isNotFound: PropTypes.bool,
   component: PropTypes.oneOfType([PropTypes.element, PropTypes.func])
-  .isRequired,
-}
+    .isRequired,
+};
 
 RouteWrapper.defaultProps = {
   isPrivate: false,
-  isNotFound: false
-}
+  isNotFound: false,
+};
