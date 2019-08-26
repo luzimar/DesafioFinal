2656208a21ac50678ca971e36bb030aff966ad46
luzimar <lzm.spfc@hotmail.com>
2019-08-26 10:12:00 -0300

    Implementado estilização do template de auth

--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { Link } from 'react-router-dom';
+import logo from '~/assets/logo.svg';
 
 export default function SignIn() {
-  return <h1>Login</h1>
+  return (
+    <>
+      <img src={logo} alt="Meetapp" />
+      <form>
+        <input type="email" placeholder="Digite seu e-mail" />
+        <input type="password" placeholder="Sua senha secreta" />
+        <button type="submit">Entrar</button>
+        <Link to="/register">Criar conta grátis</Link>
+      </form>
+    </>
+  );
 }
--- frontend/src/pages/_layouts/auth/styles.js
+++ frontend/src/pages/_layouts/auth/styles.js
@@ -1,6 +1,66 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
 export const Wrapper = styled.div`
   height: 100%;
-  background: linear-gradient(-90deg, #7159c1, #ab59c1);
+  background: linear-gradient(#22202c, #402845);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+export const Content = styled.div`
+  width: 100%;
+  max-width: 315px;
+
+  img {
+    margin: auto;
+    display: block;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    margin-top: 50px;
+
+    input {
+      background: rgba(0, 0, 0, 0.2);
+      border: 0;
+      border-radius: 4px;
+      height: 50px;
+      padding: 14px 20px;
+      color: #fff;
+      margin: 0 0 10px;
+      font-size: 18px;
+
+      &::placeholder {
+        color: gray;
+      }
+    }
+    button {
+      margin: 5px 0 0;
+      height: 50px;
+      background: #f94d6a;
+      font-weight: bold;
+      color: #fff;
+      border: 0;
+      border-radius: 4px;
+      font-size: 18px;
+      transition: background 0.2s;
+
+      &:hover {
+        background: ${darken(0.03, '#f94d6a')};
+      }
+    }
+    a {
+      text-align: center;
+      color: #fff;
+      margin-top: 20px;
+      font-size: 16px;
+      opacity: 0.6;
+
+      &:hover {
+        opacity: 1;
+      }
+    }
+  }
 `;
