57e34bee5bd40b37f3bbb9b605cedcbf32605228
luzimar <lzm.spfc@hotmail.com>
2019-08-27 20:52:16 -0300

    Configurando redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 import Routes from './routes';
 import history from '~/services/history';
 import '~/config/reactotron';
 import GlobalStyle from '~/styles/global';
+import store from './store';
 
 function App() {
   return (
-    <Router history={history}>
-      <GlobalStyle />
-      <Routes />
-    </Router>
+    <Provider store={store}>
+      <Router history={history}>
+        <GlobalStyle />
+        <Routes />
+      </Router>
+    </Provider>
   );
 }
 
---
+++ frontend/src/store/modules/auth/reducer.js
@@ -0,0 +1,9 @@
+const INITIAL_STATE = {};
+
+export default function auth(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    default:
+      console.tron.log('teste');
+      return state;
+  }
+}
