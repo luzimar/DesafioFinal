fc98f1c6501cb55a3725b5576179d9125bbf2230
luzimar <lzm.spfc@hotmail.com>
2019-10-04 21:49:51 -0300

    Criado as telas de signin e signup

---
+++ mobile/src/pages/SignIn/styles.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components/native';
+import Input from '~/components/Input';
+import Button from '~/components/Button';
+
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  padding: 0 30px;
+`;
+
+export const Form = styled.View`
+  margin-top: 50px;
+  align-self: stretch;
+`;
+
+export const FormInput = styled(Input)`
+  margin-bottom: 10px;
+`;
+
+export const SubmitButton = styled(Button)`
+  margin-top: 5px;
+`;
+
+export const SignLink = styled.TouchableOpacity`
+  margin-top: 20px;
+`;
+
+export const SignLinkText = styled.Text`
+  color: #fff;
+  font-weight: bold;
+  font-size: 16px;
+`;
---
+++ mobile/src/pages/SignUp/index.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Image } from 'react-native';
+import PropTypes from 'prop-types';
+import logo from '~/assets/logo.png';
+import Background from '~/components/Background';
+
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
+
+export default function SignUp({ navigation }) {
+  return (
+    <Background>
+      <Container>
+        <Image source={logo} />
+        <Form>
+          <FormInput
+            icon="person-outline"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Nome completo"
+          />
+
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+          />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Sua senha secreta"
+          />
+
+          <SubmitButton>Criar conta</SubmitButton>
+        </Form>
+
+        <SignLink onPress={() => navigation.navigate('SignIn')}>
+          <SignLinkText>Já tenho login</SignLinkText>
+        </SignLink>
+      </Container>
+    </Background>
+  );
+}
+
+SignUp.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func.isRequired,
+  }).isRequired,
+};
